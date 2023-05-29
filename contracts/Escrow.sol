4529196c52891bea6fc3df16861bb38668b12348
rahulpandey70 <rahulpandey9103@gmail.com>
2023-05-29 11:36:56 +0530

    Contract for lender approval

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -31,6 +31,7 @@ contract Escrow {
     mapping (uint256 => uint256) public escrowAmount;
     mapping (uint256 => address) public buyer;
     mapping (uint256 => bool) public inspectionPassed;
+    mapping (uint256 => mapping (address => bool)) public approval;
 
     constructor(
         address _nftAddress,
@@ -67,6 +68,10 @@ contract Escrow {
         onlyInspector 
     {
         inspectionPassed[_nftID] = _passed;
+    }
+
+    function approveSale(uint256 _nftID) public {
+        approval[_nftID][msg.sender] = true;
     }
 
     receive() external payable {}
