7b50718ba301938e4465c7193ec52ef687226d94
rahulpandey70 <rahulpandey9103@gmail.com>
2023-05-29 09:37:55 +0530

    Listing property

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -11,6 +11,16 @@ contract Escrow {
     address public inspector;
     address public lender;
 
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only sender can call this function");
+        _;
+    }
+
+    mapping (uint256 => bool) public isListed;
+    mapping (uint256 => uint256) public purchasePrice;
+    mapping (uint256 => uint256) public escrowAmount;
+    mapping (uint256 => address) public buyer;
+
     constructor(
         address _nftAddress,
         address payable _seller,
@@ -21,5 +31,19 @@ contract Escrow {
         seller = _seller;
         inspector = _inspector;
         lender = _lender;
+    }
+
+    function list(
+        uint256 _nftID, 
+        address _buyer, 
+        uint256 _purchasePrice, 
+        uint256 _escrowAmount
+    ) public payable onlySeller{
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        buyer[_nftID] = _buyer;
+        escrowAmount[_nftID] = _escrowAmount;
     }
 }
