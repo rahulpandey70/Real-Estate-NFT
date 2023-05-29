9907823ddafe676e472ee490bd3cf07e38397841
rahulpandey70 <rahulpandey9103@gmail.com>
2023-05-29 11:26:44 +0530

    Contract for inspection status

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -21,10 +21,16 @@ contract Escrow {
         _;
     }
 
+    modifier onlyInspector() {
+        require(msg.sender == inspector, "Only inspector can call this function");
+        _;
+    }
+
     mapping (uint256 => bool) public isListed;
     mapping (uint256 => uint256) public purchasePrice;
     mapping (uint256 => uint256) public escrowAmount;
     mapping (uint256 => address) public buyer;
+    mapping (uint256 => bool) public inspectionPassed;
 
     constructor(
         address _nftAddress,
@@ -56,9 +62,18 @@ contract Escrow {
         require(msg.value == escrowAmount[_nftID]);
     }
 
+    function updateInspectionStatus(uint256 _nftID, bool _passed) 
+        public 
+        onlyInspector 
+    {
+        inspectionPassed[_nftID] = _passed;
+    }
+
     receive() external payable {}
 
     function getBalance() public view returns(uint256) {
         return address(this).balance;
     }
+
+
 }
