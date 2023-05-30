5b1efceac50c318269d87f029d8e0d5c084b8d03
rahulpandey70 <rahulpandey9103@gmail.com>
2023-05-30 11:07:34 +0530

    Connect to metamask

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from "./abis/RealEstate.json";
+import Escrow from "./abis/Escrow.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
+	const [account, setAccount] = useState(null);
 
-  return (
-    <div>
+	// connect to metamask
+	const loadBlockchainData = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const accounts = await window.ethereum.request({
+			method: "eth_requestAccounts",
+		});
+		setAccount(accounts[0]);
+		console.log(accounts[0]);
+	};
 
-      <div className='cards__section'>
+	useEffect(() => {
+		loadBlockchainData();
+	}, []);
 
-        <h3>Welcome to Millow</h3>
-
-      </div>
-
-    </div>
-  );
+	return (
+		<div>
+			<div className="cards__section">
+				<h3>Welcome to Real Estate</h3>
+			</div>
+		</div>
+	);
 }
 
 export default App;
