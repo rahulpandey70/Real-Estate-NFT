8af9d7b56a619fa3dd92849767a1e2e400fac45a
rahulpandey70 <rahulpandey9103@gmail.com>
2023-05-30 13:22:42 +0530

    Listed all property

--- src/App.js
+++ src/App.js
@@ -14,11 +14,41 @@ import Escrow from "./abis/Escrow.json";
 import config from "./config.json";
 
 function App() {
+	const [provider, setProvider] = useState(null);
+	const [escrow, setEscrow] = useState(null);
 	const [account, setAccount] = useState(null);
+	const [homes, setHomes] = useState([]);
 
 	// connect to metamask
 	const loadBlockchainData = async () => {
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		setProvider(provider);
+
+		const network = await provider.getNetwork();
+
+		const realEstate = new ethers.Contract(
+			config[network.chainId].realEstate.address,
+			RealEstate,
+			provider
+		);
+		const totalSupply = await realEstate.totalSupply();
+		const homes = [];
+
+		for (let i = 1; i <= totalSupply; i++) {
+			const uri = await realEstate.tokenURI(i);
+			const res = await fetch(uri);
+			const metadata = await res.json();
+			homes.push(metadata);
+		}
+
+		setHomes(homes);
+
+		const escrow = new ethers.Contract(
+			config[network.chainId].escrow.address,
+			Escrow,
+			provider
+		);
+		setEscrow(escrow);
 
 		window.ethereum.on("accountsChanged", async () => {
 			const accounts = await window.ethereum.request({
@@ -36,8 +66,29 @@ function App() {
 	return (
 		<div>
 			<Navigation account={account} setAccount={setAccount} />
+			<Search />
+
 			<div className="cards__section">
-				<h3>Welcome to Real Estate</h3>
+				<h3>Homes</h3>
+				<hr />
+				<div className="cards">
+					{homes.map((home, index) => (
+						<div className="card" key={index}>
+							<div className="card__image">
+								<img src={home.image} alt={home.image} />
+							</div>
+							<div className="card__info">
+								<h4>{home.attributes[0].value} ETH</h4>
+								<p>
+									<strong>{home.attributes[2].value}</strong> bds |
+									<strong>{home.attributes[3].value}</strong> ba |
+									<strong>{home.attributes[4].value}</strong> sqrt
+								</p>
+								<p>{home.address}</p>
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	);
