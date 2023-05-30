bddcdeac4cc54fd114fa92b7b1b655ac626e8a88
rahulpandey70 <rahulpandey9103@gmail.com>
2023-05-30 11:31:37 +0530

    FInish Navbar

--- src/App.js
+++ src/App.js
@@ -19,11 +19,14 @@ function App() {
 	// connect to metamask
 	const loadBlockchainData = async () => {
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
-		const accounts = await window.ethereum.request({
-			method: "eth_requestAccounts",
+
+		window.ethereum.on("accountsChanged", async () => {
+			const accounts = await window.ethereum.request({
+				method: "eth_requestAccounts",
+			});
+			const account = ethers.utils.getAddress(accounts[0]);
+			setAccount(account);
 		});
-		setAccount(accounts[0]);
-		console.log(accounts[0]);
 	};
 
 	useEffect(() => {
@@ -32,6 +35,7 @@ function App() {
 
 	return (
 		<div>
+			<Navigation account={account} setAccount={setAccount} />
 			<div className="cards__section">
 				<h3>Welcome to Real Estate</h3>
 			</div>
